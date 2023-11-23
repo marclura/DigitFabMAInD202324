12813fe1ef6eff4e437be27f359f110761689fef
marclura <marco@marcolurati.ch>
2023-11-23 11:02:49 +0100

    projects setup

---
+++ DigiFabMAInD_202324-Marta_Alessandro - Web/DigiFabMAInD_202324-Marta_Alessandro - Web.ino
@@ -0,0 +1,51 @@
+/*
+ * Master of Arts in Interaction Design https://maind.supsi.ch/
+ * University of Applied Sciences and Arts of Southern Switzerland, SUPSI
+ * 
+ * Course: Digitally Designed Objects for Fast Prototyping
+ * Teacher: Marco Lurati, marco.lurati@supsi.ch
+ * Assistant: Matteo Subet, matteo.subet@supsi.ch
+ * Year: 2023-24 
+ *
+ * Group: Marta and Alessandro
+ * Project title: Web
+ *
+ * MAInD projects repository: https://www.notion.so/7522b850bb0745e09931cc34d0566dc3?v=6d5569e2efa243298f5d8739226356b7&pvs=4
+ * 
+ * Board used: ML042 - 20221031
+ * http://marcolurati.ch
+ *
+ *
+ * Required libraries:
+ * ESP32-BLE-Keyboard https://github.com/T-vK/ESP32-BLE-Keyboard
+ * ML042_Figma_Lib https://github.com/marclura/ML042_Figma_Lib
+ *
+ */
+
+#include <BleKeyboard.h>
+#include <ML042FigmaLib.h>
+
+
+BleKeyboard bleKeyboard;
+
+void setup() {
+
+  // serial USB communication
+  Serial.begin(115200);
+
+  // keyboard setup
+  bleKeyboard.setName("Web M+A");  // name of the device not too long else iOS won't see it
+  bleKeyboard.begin();
+
+  delay(500);
+
+
+
+}
+
+void loop() {
+
+
+
+
+}
