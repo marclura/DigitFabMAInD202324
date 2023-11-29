742e7e635af87a661cd3989b23b51e03178ceee1
marclura <marco@marcolurati.ch>
2023-11-29 20:03:46 +0100

    update Podcast

--- DigiFabMAInD_202324-Kristina_Vittorio_Vittorio_Radio/DigiFabMAInD_202324-Kristina_Vittorio_Vittorio_Radio.ino
+++ DigiFabMAInD_202324-Kristina_Vittorio_Vittorio_Radio/DigiFabMAInD_202324-Kristina_Vittorio_Vittorio_Radio.ino
@@ -20,6 +20,10 @@
  * ESP32-BLE-Keyboard https://github.com/T-vK/ESP32-BLE-Keyboard
  * ML042_Figma_Lib https://github.com/marclura/ML042_Figma_Lib
  *
+ *
+ * Note:
+ * The key (char) for Figma has to be "lowercase" to work!!
+ *
  */
 
 #include <BleKeyboard.h>
@@ -28,13 +32,16 @@
 BleKeyboard bleKeyboard;
 
 // buttons
-FigmaButton next(13, 'N');
-FigmaButton back(14, 'K');
+FigmaButton next(33, 'g');
+FigmaButton back(35, 'h');
 
 // potentiometer
 FigmaPot filter(2, 4, 100);
 FigmaPot volume(13, 4, 100);
-FigmaPot status(14, 3, 200);
+FigmaPot status(27, 3, 200);
+
+// Light sensor
+FigmaLightSensor cover(26);
 
 
 void setup() {
@@ -49,19 +56,22 @@ void setup() {
   delay(500);
 
   // potentiometer mapping
-  filter.addPosition(1, 512, 'A');
-  filter.addPosition(2, 1536, 'B');
-  filter.addPosition(3, 2560, 'C');
-  filter.addPosition(4, 3584, 'D');
+  filter.addPosition(1, 512, 'a');
+  filter.addPosition(2, 1536, 'b');
+  filter.addPosition(3, 2560, 'c');
+  filter.addPosition(4, 3584, 'd');
 
   volume.addPosition(1, 512, '1');
   volume.addPosition(2, 1536, '2');
   volume.addPosition(3, 2560, '3');
   volume.addPosition(4, 3584, '4');
 
-  status.addPosition(1, 682, 'P');  // podcast
-  status.addPosition(2, 2048, 'F'); // off
-  status.addPosition(3, 3412, 'R'); // loop/radio
+  status.addPosition(1, 682, 'x');  // clock
+  status.addPosition(2, 2048, 'y'); // podcast
+  status.addPosition(3, 3412, 'z'); // random mode
+
+  // light sensor
+  cover.triggerThreshold(2000, 100, 'k', 'l');
 
 }
 
@@ -70,25 +80,38 @@ void loop() {
   // update
   next.update();
   back.update();
+  cover.update();
 
   // status
   // buttons
   if(next.pressed()) {
     Serial.println("Next pressed, key: " + String(next.key()));
+    if(bleKeyboard.isConnected()) bleKeyboard.write(next.key());
   }
   if(back.pressed()) {
     Serial.println("Back pressed, key: " + String(back.key()));
+    if(bleKeyboard.isConnected()) bleKeyboard.write(back.key());
   }
 
   // potentiometer
   if(filter.changed()) {
     Serial.println("Filter changed, key: " + String(filter.key()));
+    if(bleKeyboard.isConnected()) bleKeyboard.write(filter.key());
   }
   if(volume.changed()) {
     Serial.println("Volume changed, key: " + String(volume.key()));
+    if(bleKeyboard.isConnected()) bleKeyboard.write(volume.key());
   }
   if(status.changed()) {
     Serial.println("Status changed, key: " + String(status.key()));
+    if(bleKeyboard.isConnected()) bleKeyboard.write(status.key());
+  }
+
+  // light sensor
+  if(cover.changed()) {
+    Serial.println("Cover status changed, key: " + String(cover.key()));
+    if(bleKeyboard.isConnected()) bleKeyboard.write(cover.key());
+    
   }
 
   delay(5);
